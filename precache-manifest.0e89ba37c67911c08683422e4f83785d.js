82f6ea1eadbf4cfe82bbadaadf3013519ab38d56
Roi-Ravia <roi.ravia95@gmail.com>
2020-06-03 11:55:32 +0300

    Updates

---
+++ precache-manifest.0e89ba37c67911c08683422e4f83785d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a45bd59fe436b84913301d0c251633c9",
+    "url": "/job-list/index.html"
+  },
+  {
+    "revision": "76123bdcede998973ced",
+    "url": "/job-list/static/js/2.05d3c385.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/job-list/static/js/2.05d3c385.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74a0fe73572976156dd",
+    "url": "/job-list/static/js/main.1b348374.chunk.js"
+  },
+  {
+    "revision": "919f8edb7fa5a5680156",
+    "url": "/job-list/static/js/runtime-main.086e3b47.js"
+  }
+]);
